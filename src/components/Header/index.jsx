25d48256ac2f87f483ccf8d753c123920af233de
way99 <745855589@qq.com>
2022-01-28 20:55:19 +0800

    feat add 头部组件功能实现3-7_8

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,58 @@
-import React, { Component } from 'react'
+import { Col, Row } from 'antd';
+import React, { Component } from 'react';
+import './index.less';
+import Util from '../../utils/utils';
+import axios from 'axios'; //引入第三方包的
 
 export default class Header extends Component {
+    // state={}
+    componentWillMount(){
+        //用户信息
+        this.setState({
+            userName:'段嘉许'
+        });
+        //时间
+        setInterval(()=>{
+            let sysTime=Util.formateDate(new Date().getTime());
+            this.setState({
+                sysTime
+            });
+        },1000)
+        //调用天气
+        this.getWeatherAPIDate();
+    }
+
+    //获取天气
+    getWeatherAPIDate(){
+        axios.get("https://devapi.qweather.com/v7/weather/now?location=101090201&key=ab5bcf08250f47ab8a4fd1745cd58f66")
+        .then((res)=>{
+            console.log(res)
+            let data=res.data;
+            data=data.now.text;
+            if(res.data.code==="200"){
+                this.setState({weather:data})
+            }
+        })
+    }
+
     render() {
         return (
-            <div>
-                Header
+            <div className='header'>
+                <Row className='header-top'>
+                    <Col span="24">
+                        <span>欢迎，{this.state.userName}</span>
+                        <a href='#'>退出</a>
+                    </Col>
+                </Row>
+                <Row className='breadcrumb'>
+                    <Col span="4" className='breadcrumb-title'>
+                        首页
+                    </Col>
+                    <Col span="20" className='weather'>
+                        <span className='date'>{this.state.sysTime}</span>
+                        <span className='weather-detail'>{this.state.weather}</span>
+                    </Col>
+                </Row>
             </div>
         )
     }
--- src/components/NavLeft/index.jsx
+++ src/components/NavLeft/index.jsx
@@ -1,10 +1,37 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Menu } from 'antd';
+import MenuConfig from '../../config/menuConfig';
+import './index.less';
 
+const { SubMenu } = Menu;
 export default class NavLeft extends Component {
+    componentWillMount(){
+        const menuTreeNode=this.renderMenu(MenuConfig);
+        this.setState({menuTreeNode});
+    }
+    //菜单渲染————递归遍历
+    renderMenu=(data)=>{
+        return data.map(item=>{
+            if(item.children){
+                return (
+                    <SubMenu title={item.title} key={item.key}>
+                        {this.renderMenu(item.children)}
+                    </SubMenu>
+                )
+            }
+            return <Menu.Item key={item.key} >{item.title}</Menu.Item>
+        })
+    }
     render() {
         return (
             <div>
-                NavLeft
+                <div className='logo'>
+                    <img src="/assets/logo.svg" alt="" />
+                    <h1>DC MS</h1>
+                </div>
+                <Menu theme="dark">
+                    {this.state.menuTreeNode}
+                </Menu>
             </div>
         )
     }
