0b560a8f3bd8d38c7e3c97d829232af185d8c11a
way99 <745855589@qq.com>
2022-01-29 13:45:43 +0800

    feat add 底部组件功能实现3-9

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './index.less';
 
 export default class Footer extends Component {
     render() {
         return (
-            <div>
-                Footer
+            <div className='footer'>
+                版权所有：慕课网&段嘉许 (推荐使用谷歌浏览器，可以获得更佳操作页面体验) 技术支持：段老狗
             </div>
         )
     }
